85d6b13c94b25c127bf76be8b57ed48fb104391e
andrej fritz <a.fritz@betzold.de>
2014-09-10 16:31:37 +0200

    update to meteor 0.9

--- package.js
+++ package.js
@@ -1,8 +1,11 @@
 Package.describe({
-  summary: "Tinycon, manipulating the favicon"
+  summary: "Tinycon, manipulating the favicon",
+  version: "0.0.1",
+  git: "https://github.com/andruschka/meteor-tinycon.git"
 });
 
 Package.on_use(function(api) {
+  api.versionsFrom("METEOR@0.9.0");
   api.add_files(
     [
       'tinycon.min.js'
